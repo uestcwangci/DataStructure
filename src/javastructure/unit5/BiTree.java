17e4e36248843fdd995de83a20f9065763af28dd
UESTC_Forgetter <420197925@qq.com>
2018-10-08 22:14:53 +0800

    第五章，二叉树应用实例

--- src/javastructure/unit5/BiTree.java
+++ src/javastructure/unit5/BiTree.java
@@ -3,7 +3,13 @@ package javastructure.unit5;
 import javastructure.unit3.queue.LinkQueue;
 import javastructure.unit3.stack.LinkStack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class BiTree {
+    public int count = 0;
+
     private BiTreeNode root;
 
     public BiTree() {
@@ -140,6 +146,86 @@ public class BiTree {
                 }
             }
         }
+    }
+
+    public BiTreeNode searchNode(BiTreeNode T, Object x) {
+        if (T != null) {
+            if (T.data.equals(x)) {
+                return T;
+            } else {
+                BiTreeNode result = searchNode(T.lChild, x);
+                return result == null ? searchNode(T.rChild, x) : result;
+            }
+        }
+        return null;
+    }
+
+    public void countNode(BiTreeNode T) {
+        if (T != null) {
+            count++;
+            countNode(T.lChild);
+            countNode(T.rChild);
+        }
+    }
+
+    public void countNode1(BiTreeNode T) throws Exception {
+        if (T != null) {
+            LinkQueue Q = new LinkQueue();
+            Q.offer(T);
+            while (!Q.isEmpty()) {
+                T = (BiTreeNode) Q.poll();
+                count++;
+                if (T.lChild != null) {
+                    Q.offer(T.lChild);
+                }
+                if (T.rChild != null) {
+                    Q.offer(T.rChild);
+                }
+            }
+        }
+    }
+
+    public List<Object> traverse(BiTreeNode root){
+        List<Object> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<BiTreeNode> s = new Stack<>();
+        while (root != null || !s.isEmpty()) {
+            while (root != null) {
+                s.push(root);
+                root = root.lChild;
+            }
+            BiTreeNode t = s.pop();
+            list.add(t.data);
+            root = t.rChild;
+        }
+        return list;
+    }
+
+    public int getDepth(BiTreeNode T) {
+        if (T != null) {
+            int lDepth = getDepth(T.lChild);
+            int rDepth = getDepth(T.rChild);
+            return 1 + (lDepth > rDepth ? lDepth : rDepth);
+        }
+        return 0;
+    }
+
+    public boolean isEqual(BiTreeNode T1, BiTreeNode T2) {
+        if (T1 == null && T2 == null) {
+            return true;
+        }
+        if (T1 != null && T2 != null) {
+            if (T1.data == T2.data) {
+                if (isEqual(T1.lChild, T2.lChild)) {
+                    if (isEqual(T1.rChild, T2.rChild)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public BiTreeNode getRoot() {
--- src/javastructure/unit5/DebugBiTree.java
+++ src/javastructure/unit5/DebugBiTree.java
@@ -1,5 +1,7 @@
 package javastructure.unit5;
 
+import java.lang.reflect.Array;
+
 public class DebugBiTree {
     public static void main(String[] args) throws Exception {
         DebugBiTree debug = new DebugBiTree();
@@ -30,6 +32,22 @@ public class DebugBiTree {
         System.out.print("层次遍历序列为：");
         tree.levelTraverse();
         System.out.println();
+        //计数
+        tree.count = 0;
+        tree.countNode(root);
+        System.out.println(tree.count);
+        //遍历
+        System.out.println(tree.traverse(root).toString());
+        //寻找结点
+        System.out.println(tree.searchNode(root, 'F').data);
+        //计数
+        tree.count = 0;
+        tree.countNode1(root);
+        System.out.println(tree.count);
+        //深度
+        System.out.println("深度:" + tree.getDepth(root));
+
+
     }
 
     private BiTree createBiTree() {
